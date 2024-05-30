4218776f9758ac17699e230a9f42720e8d3aeec7
Naufal Farras <mnfm95@gmail.com>
2024-05-30 07:17:11 +0700

    [ADD] farmer dao model

---
+++ models/dao/farmer.go
@@ -0,0 +1,16 @@
+package dao
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type Farmer struct {
+	bun.BaseModel `bun:"farmers"`
+	ID            uint64    `bun:"id,pk,autoincrement"`
+	Name          string    `bun:"name,notnull"`
+	BirthDate     time.Time `bun:"birth_date,notnull"`
+	CreatedAt     time.Time `bun:"created_at,notnull"`
+	UpdatedAt     time.Time `bun:"updated_at,notnull"`
+}
