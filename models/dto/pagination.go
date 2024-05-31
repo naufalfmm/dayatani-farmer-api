fc4523ec0a0483fda7a1131d5af8f08c4d85e2a6
Naufal Farras <mnfm95@gmail.com>
2024-05-31 09:46:22 +0700

    [ADD] sort in pagination req

--- models/dto/pagination.go
+++ models/dto/pagination.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/naufalfmm/dayatani-farmer-api/consts"
 	"github.com/naufalfmm/dayatani-farmer-api/utils/helper"
@@ -10,12 +12,14 @@ type PaginationRequest struct {
 	Page   int
 	Limit  int
 	Offset int
+	Sorts  []string
 }
 
 func (req *PaginationRequest) FromGinContext(gc *gin.Context) {
 	req.Page = helper.ConvertStringDefault(gc.Param("page"), consts.DefPage)
 	req.Limit = helper.ConvertStringDefault(gc.Param("limit"), consts.DefLimit)
 	req.Offset = helper.ConvertStringDefault(gc.Param("offset"), consts.DefOffset)
+	req.Sorts = strings.Split(gc.Param("sorts"), ",")
 
 	if req.Limit > consts.MaxLimit {
 		req.Limit = consts.DefLimit
